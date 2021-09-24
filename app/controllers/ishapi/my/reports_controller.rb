236996b6a1d58604f7d7e2f53b93232ce2fb3bd5
piousbox personal-mac <piousbox@gmail.com>
2021-09-24 18:46:36 -0500

    autobuild `date "+%Y%m%d"`

--- app/controllers/ishapi/my/reports_controller.rb
+++ app/controllers/ishapi/my/reports_controller.rb
@@ -1,14 +1,11 @@
 
-module Ishapi
-  module My
-    class ReportsController < Ishapi::My::MyController
 
-      def index
-        authorize! :my_index, Report
-        @reports = @profile.reports
-      end
+class Ishapi::My::ReportsController < Ishapi::ApplicationController
 
-    end
+  def index
+    authorize! :my_index, Report
+    @reports = @profile.reports
   end
+
 end
 
--- app/controllers/ishapi/my/videos_controller.rb
+++ app/controllers/ishapi/my/videos_controller.rb
@@ -1,14 +1,10 @@
 
-module Ishapi
-  module My
-    class VideosController < Ishapi::My::MyController
+class Ishapi::My::VideosController < Ishapi::ApplicationController
 
-      def index
-        authorize! :my_index, Video
-        @videos = @current_user.profile.videos.unscoped.where( is_trash: false ).order_by( created_at: :desc ).limit(20)
-      end
-
-    end
+  def index
+    authorize! :my_index, Video
+    @videos = @current_user.profile.videos.unscoped.where( is_trash: false ).order_by( created_at: :desc ).limit(20)
   end
+
 end
 
