acea698e0fe334005aaa699702c242c50c8e7170
mac_a2141 <victor@piousbox.com>
2023-01-11 20:52:11 -0600

    autobuild `date "+%Y%m%d"`

--- app/controllers/ishapi/maps_controller.rb
+++ app/controllers/ishapi/maps_controller.rb
@@ -5,6 +5,8 @@ class Ishapi::MapsController < Ishapi::ApplicationController
   before_action :check_profile, only: [ :show ]
 
   def show
+    puts! @current_profile, '@current_profile'
+
     if 'self' == params[:slug] # @TODO: constantize _vp_ 2023-01-11
       @location = ::Gameui::Map.where( slug: @current_profile[:email] ).first
     else
