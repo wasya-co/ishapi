22aab4b9c2db839b9e75617b39d2b736b3e00030
mac_a2141 <victor@piousbox.com>
2023-03-30 01:22:41 -0500

    autobuild `date "+%Y%m%d"`

--- app/controllers/ishapi/payments_controller.rb
+++ app/controllers/ishapi/payments_controller.rb
@@ -3,52 +3,58 @@ class Ishapi::PaymentsController < ::Ishapi::ApplicationController
 
   before_action :check_profile, only: %i| create unlock |
 
-  ## Hmmm, ishapi does not verify authenticity token.
-  # skip_before_action :verify_authenticity_token, only: %i| create stripe_confirm |
+  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, only: %i| stripe_confirm |
 
   # alphabetized : )
 
   ## _vp_ 2020-07-21 This is for guyd
   ## _vp_ 2022-03-01 It's been a while!
   ## _vp_ 2022-09-04 continue
+  ## _vp_ 2023-03-29 Continue
   ##
   ## @TODO: cannot proceed if already is_purchasing?
   ## @TODO: and this doesn't say what you're buying! herehere
   ##
   def create
-    authorize! :create, ::Ish::Payment
+    authorize! :create, Ish::Payment.new
 
     @current_profile.update_attributes({ is_purchasing: true })
 
-    begin
-      amount_cents  = params[:amount_cents].to_i # @TODO: change
+    amount_cents  = params[:amount_cents].to_i # @TODO: change
 
-      ::Stripe.api_key = ::STRIPE_SK
-      intent = Stripe::PaymentIntent.create({
-        amount: amount_cents,
-        currency: 'usd',
-        metadata: { integration_check: "accept_a_payment" },
-      })
+    ::Stripe.api_key = ::STRIPE_SK
+    intent = Stripe::PaymentIntent.create({
+      amount: amount_cents,
+      currency: 'usd',
+      metadata: { integration_check: "accept_a_payment" },
+    })
 
-      payment = Ish::Payment.create!(
-        client_secret: intent.client_secret,
-        email: @current_profile.email,
-        payment_intent_id: intent.id,
-        profile_id: @current_profile.id,
-      )
-      render json: {
-        client_secret: intent.client_secret,
-        clientSecret: intent.client_secret,
-      }
-
-    rescue Mongoid::Errors::DocumentNotFound => e
-      puts! e, '#create2 Mongoid::Errors::DocumentNotFound'
-      render :status => 404, :json => e
+    item_type = case params[:item_type]
+    when 'Ish::Location'
+      'Gameui::Map'
+    else
+      throw "item_type not implemented: #{params[:item_type]}"
     end
+    item = item_type.constantize.find params[:item_id]
+
+    payment = Ish::Payment.create!(
+      client_secret: intent.client_secret,
+      email: @current_profile.email,
+      item: item,
+      payment_intent_id: intent.id,
+      profile_id: @current_profile.id,
+    )
+    render json: {
+      client_secret: intent.client_secret,
+      clientSecret: intent.client_secret,
+    }
+
   end
 
   ##
   ## webhook
+  ## _vp_ 2023-03-29 Continue
   ##
   def stripe_confirm
     authorize! :open_permission, ::Ishapi
@@ -63,11 +69,12 @@ class Ishapi::PaymentsController < ::Ishapi::ApplicationController
     end
 
     payment_intent = event.data.object
+    puts! payment_intent, 'payment_intent'
 
     payment = Ish::Payment.where( payment_intent_id: payment_intent.id ).first
     if payment && payment_intent['status'] == 'succeeded'
 
-      payment.update_attributes( status: :confirmed )
+      payment.update_attributes( status: Ish::Payment::STATUS_CONFIRMED )
       n_unlocks = payment.profile.n_unlocks + 1 # @TODO: it's not always 5, adjust! herehere
 
       payment.profile.update_attributes!( n_unlocks: n_unlocks, is_purchasing: false )
